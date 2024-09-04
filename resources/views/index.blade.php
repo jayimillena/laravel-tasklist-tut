154eae86177f7bd6dc09eebc7b4571e54bc5246b
Jay Millena <jay.millena07@gmail.com>
2024-09-04 12:10:54 +0800

    Blade directves

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,13 @@
-Hello {{ $name }}
+<div>
+    Hello I'm a blade template!
+</div>
+
+<div>
+    @if(count($tasks))
+        @foreach ($tasks as $task)
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        @endforeach
+    @else
+        <div>There are no task!</div>
+    @endif
+</div>

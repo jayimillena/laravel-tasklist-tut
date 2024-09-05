237ec51eeac7ffa61a0b7c1c20cc7038da01fed0
Jay Millena <jay.millena07@gmail.com>
2024-09-05 20:40:20 +0800

    Layouts using template inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,18 @@
-<div>
-    Hello I'm a blade template!
-</div>
+@extends('layouts.app')
 
-<div>
-    @if(count($tasks))
-        @foreach ($tasks as $task)
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-        @endforeach
-    @else
-        <div>There are no task!</div>
-    @endif
-</div>
+@section('title', 'Laravel Tasks List')
+@section('content')
+    <div>
+        Hello I'm a blade template!
+    </div>
+
+    <div>
+        @if(count($tasks))
+            @foreach ($tasks as $task)
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            @endforeach
+        @else
+            <div>There are no task!</div>
+        @endif
+    </div>
+@endsection
